a891d0a42b1827f7d546077d3e421785b361856d
Craig Shoemaker <craig@craigshoemaker.net>
2016-05-12 16:12:27 -0700

    implement filter pipe

--- app/movies/movies-list.component.ts
+++ app/movies/movies-list.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 import { IMovie } from './movie.interface';
 import { MoviesService } from './movies.service';
+import { MoviesFilterPipe } from './movies-filter.pipe';
 
 @Component({
     selector: 'movies-list',
-    templateUrl: 'app/movies/movies-list.component.html'
+    templateUrl: 'app/movies/movies-list.component.html',
+    pipes: [MoviesFilterPipe]
 })
 export class MoviesListComponent implements OnInit {
     
+    filterText: string = '';
     movies: IMovie[] = [];
     
     constructor(private _moviesService: MoviesService) { }
