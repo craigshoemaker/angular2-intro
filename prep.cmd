d7374433cb2a27d3858f61c56d4031ba06a45e35
Craig Shoemaker <craig@craigshoemaker.net>
2016-05-16 14:15:05 -0700

    changed to use git clone

--- prep.cmd
+++ prep.cmd
@@ -1,16 +1,5 @@
-xcopy /I .vscode _demo\.vscode
-xcopy /I app _demo\app
-xcopy /I img _demo\img
-xcopy /I typings _demo\typings
-
-xcopy *.html _demo
-xcopy *.js _demo
-xcopy *.css _demo
-xcopy *.json _demo
-
-copy edit.cmd _demo
-copy start.cmd _demo
-
 cd _demo
+
+git clone -b demo --single-branch https://github.com/craigshoemaker/angular2-intro.git .
 
 npm install
